2f4b070a59debf73cdc18a4b8cedd671220cd1d2
Diego582 <penaomardiego@gmail.com>
2026-03-16 11:41:57 -0300

    periodo

--- controllers/factura/destroy.js
+++ controllers/factura/destroy.js
@@ -1,8 +1,18 @@
 import Factura from "../../models/Factura.js";
+import ItemFactura from "../../models/ItemFactura.js";
+
 
 export default async (req, res, next) => {
   try {
-    let deletedInvoice = await Factura.findByIdAndDelete(req.params.id);
+    const { id } = req.params;
+
+
+
+    let itemfacturadelete = await ItemFactura.deleteMany({ factura_id: id });
+
+
+
+    let deletedInvoice = await Factura.findByIdAndDelete(id);
 
     if (deletedInvoice) {
       return res.status(200).json({
